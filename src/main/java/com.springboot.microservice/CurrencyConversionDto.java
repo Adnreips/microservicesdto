f5948735d90fe3a92813b739fe0fcbb35b1df15e
Andrei <109a@mail.ru>
2021-04-16 15:30:34 +0300

    Changed id to Long

--- src/main/java/com.springboot.microservice/CurrencyConversionDto.java
+++ src/main/java/com.springboot.microservice/CurrencyConversionDto.java
@@ -3,11 +3,10 @@ package com.springboot.microservice;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.UUID;
+import java.util.Objects;
 
 public class CurrencyConversionDto implements Serializable {
-    static final long SerialVersionUID = -1;
-    private UUID id;
+    private Long id;
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
@@ -15,7 +14,10 @@ public class CurrencyConversionDto implements Serializable {
     private BigDecimal multiply;
     private int port;
 
-    public CurrencyConversionDto(UUID id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal multiply, int port) {
+    public CurrencyConversionDto() {
+    }
+
+    public CurrencyConversionDto(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal multiply, int port) {
         this.id = id;
         this.from = from;
         this.to = to;
@@ -25,11 +27,11 @@ public class CurrencyConversionDto implements Serializable {
         this.port = port;
     }
 
-    public UUID getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -79,5 +81,31 @@ public class CurrencyConversionDto implements Serializable {
 
     public void setPort(int port) {
         this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CurrencyConversionDto that = (CurrencyConversionDto) o;
+        return port == that.port && Objects.equals(id, that.id) && Objects.equals(from, that.from) && Objects.equals(to, that.to) && Objects.equals(conversionMultiple, that.conversionMultiple) && Objects.equals(quantity, that.quantity) && Objects.equals(multiply, that.multiply);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, conversionMultiple, quantity, multiply, port);
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyConversionDto{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionMultiple=" + conversionMultiple +
+                ", quantity=" + quantity +
+                ", multiply=" + multiply +
+                ", port=" + port +
+                '}';
     }
 }
